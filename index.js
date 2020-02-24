393d8bfedab31e4fee6381466cdd3ebf8fea0da7
Nethelbert <gilblasse@gmail.com>
2020-02-24 13:06:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue(driver,k,v) {
-  k = {}
-  let newDriver = Object.assign(driver,k[v]);
+  let newDriver = Object.assign(driver,{k: v});
   return newDriver
 }
 
